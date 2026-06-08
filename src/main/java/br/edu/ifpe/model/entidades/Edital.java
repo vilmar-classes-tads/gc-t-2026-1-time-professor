b401eeabeff155248e3b7f3d625dc0d34b8821aa
Vilmar Nepomuceno <vilmarnepomuceno@recife.ifpe.edu.br>
2026-06-08 10:08:32 -0300

    #2 config inicial

---
+++ src/main/java/br/edu/ifpe/model/entidades/Edital.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.model.entidades;
+
+public class Edital {
+
+    private String titulo;
+    private String descricao;
+
+
+    public Edital(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+}
